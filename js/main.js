14b53670b225d49820585e018da5451cfe1651a9
Alin Lupascu <lupascu.marius@gmail.com>
2021-01-14 12:56:26 -0700

    go

--- js/main.js
+++ js/main.js
@@ -18,51 +18,20 @@
 
 })(jQuery);
 
-function measureBMI () {
-	// true = metric, false = imperial
-	let unit = document.getElementById("bmi-metric").checked,
-		weight = document.getElementById("bmi-weight"),
-		weightu = document.getElementById("bmi-weight-unit"),
-		height = document.getElementById("bmi-height"),
-		heightu = document.getElementById("bmi-height-unit");
    
-	if (unit) {
-	  weightu.innerHTML = "KG";
-	  weight.min = 1;
-	  weight.max = 635;
-	  heightu.innerHTML = "CM";
-	  height.min = 54;
-	  height.max = 272;
-	} else {
-	  weightu.innerHTML = "LBS";
-	  weight.min = 2;
-	  weight.max = 1400;
-	  heightu.innerHTML = "IN";
-	  height.min = 21;
-	  height.max = 107;
-	}
-  }
-   
-  function calcBMI () {
+function calcBMI () {
 	// (A) Get elements
 	let bmi = null,
-		unit = document.getElementById("bmi-metric").checked, // true = metric, false = imperial
 		weight = parseInt(document.getElementById("bmi-weight").value),
-		height = parseInt(document.getElementById("bmi-height").value),
+		height_ft = parseInt(document.getElementById("bmi-height-ft").value),
+		height_in = parseInt(document.getElementById("bmi-height-in").value),
 		results = document.getElementById("bmi-results");
-   
-	// (B) Calculate BMI
-	// Metric BMI = Mass (kg) / Height (m) square 
-	if (unit) {
-	  height = height / 100;
-	  bmi = weight / (height * height);
-	}
-	// Imperial BMI = 703 X Mass (lbs) / Height (in) square 
-	else {
+
+		// Imperial Formula
+		height = (height_ft * 12) + height_in
 	  bmi = 703 * (weight / (height * height));
-	}
-	// Round off
-	bmi = Math.round(bmi * 100) / 100; // Round off 2 decimal places
+
+	bmi = Math.round(bmi * 10) / 10; // Round off 1 decimal place
    
 	// (C) Show Results
 	if (bmi < 18.5) {
